cbbf0053bff145f70ee2d37ff73bb6ac56def45a
Miles Pernicious <miles@milespernicious.com>
2023-01-29 00:26:07 -1000

    Allow setting title and description on a page level

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+/*
+ * (C) Copyright 2023 Miles Pernicious
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+export const SiteConfig = {
+	title: "Miles Pernicious",
+	description: "Miles Pernicious is a family-friendly content creator on Twitch. Miles plays Minecraft, piano, and uses Linux.",
+	titleSeparator: '|',
+}
