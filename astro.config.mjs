607b56a4446edf4998738d7e9e6b93d17e88ab62
Miles Pernicious <miles@milespernicious.com>
2023-02-15 20:17:49 -1000

    Add official sitemap integration @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.milespernicious.com',
-	integrations: [mdx()],
+	integrations: [
+		mdx(),
+		sitemap(),
+	],
 	publicDir: "./assets",
 	vite: {
 		define: {
