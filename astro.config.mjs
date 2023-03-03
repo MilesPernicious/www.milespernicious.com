d9ffff6bd3b4fe40ecb1d7f35a70ab681dd1a324
Miles Pernicious <miles@milespernicious.com>
2023-03-02 22:57:53 -1000

    Run PurgeCSS for production builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import purgeCss from 'astro-purgecss';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
 		sitemap({
 			filter: (page) => page !== 'https://www.milespernicious.com/contact/mahalo',
 		}),
+		purgeCss(),
 	],
 	publicDir: "./assets",
 	vite: {
